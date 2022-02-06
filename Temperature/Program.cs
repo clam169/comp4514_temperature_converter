eebe1b315b9e4528717940387cdecc18d51c6f32
Jo Jo Lam <jojilam@gmail.com>
2022-02-06 01:20:11 -0800

    pusssh

---
+++ Temperature/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Temperature;
+
+Conversion c = new Conversion();
+
+double t;
+double result;
+
+// Celsius_to_Fahrenheit
+t = 0d;
+result = c.Convert(Conversion.ConversionMode.Celsius_to_Fahrenheit, t);
+Console.WriteLine($"{t} celcius is {result} fahrenheit");
+
+t = 100d;
+result = c.Convert(Conversion.ConversionMode.Celsius_to_Fahrenheit, t);
+Console.WriteLine($"{t} celcius is {result} fahrenheit");
+
+//Kelvin_to_Fahrenheit
+t = 0d;
+result = c.Convert(Conversion.ConversionMode.Kelvin_to_Fahrenheit, t);
+Console.WriteLine($"{t} kelvin is {result} fahrenheit");
+
+t = 100d;
+result = c.Convert(Conversion.ConversionMode.Kelvin_to_Fahrenheit, t);
+Console.WriteLine($"{t} kelvin is {result} fahrenheit");
+
+//Fahrenheit_to_Celcius
+t = 32d;
+result = c.Convert(Conversion.ConversionMode.Fahrenheit_to_Celsius, t);
+Console.WriteLine($"{t} fahrenheit is {result} celcius");
+
+t = 100d;
+result = c.Convert(Conversion.ConversionMode.Fahrenheit_to_Celsius, t);
+Console.WriteLine($"{t} fahrenheit is {result} celcius");
+
+// Celsius_to_Kelvin
+t = 0d;
+result = c.Convert(Conversion.ConversionMode.Celsius_to_Kelvin, t);
+Console.WriteLine($"{t} celcius is {result} kelvin");
+
+t = 100d;
+result = c.Convert(Conversion.ConversionMode.Celsius_to_Kelvin, t);
+Console.WriteLine($"{t} celcius is {result} kelvin");
+
+//Kelvin_to_Fahrenheit
+t = 0d;
+result = c.Convert(Conversion.ConversionMode.Kelvin_to_Celsius, t);
+Console.WriteLine($"{t} kelvin is {result} celcius");
+
+t = 100d;
+result = c.Convert(Conversion.ConversionMode.Kelvin_to_Celsius, t);
+Console.WriteLine($"{t} kelvin is {result} celcius");
+
+//Fahrenheit_to_Kelvin
+t = 0d;
+result = c.Convert(Conversion.ConversionMode.Fahrenheit_to_Kelvin, t);
+Console.WriteLine($"{t} fahrenheit is {result} kelvin");
+
+t = 100d;
+result = c.Convert(Conversion.ConversionMode.Fahrenheit_to_Kelvin, t);
+Console.WriteLine($"{t} fahrenheit is {result} kelvin");
---
+++ TemperatureClient/Pages/Index.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Temperature;
+
+namespace TemperatureClient.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+    [BindProperty]
+    public double InputValue { get; set; }
+    [BindProperty]
+    public double ConvertedValue { get; set; }
+    [BindProperty]
+    public string ConvertFrom { get; set; }
+    [BindProperty]
+    public string ConvertTo { get; set; }
+
+    public void OnPost() {
+      Conversion c = new Conversion();
+      
+      if(ConvertTo.Equals(ConvertFrom)) {
+        ConvertedValue = InputValue;
+      } else {
+        if(ConvertFrom.Equals("Celcius")) {
+          if (ConvertTo.Equals("Fahrenheit")) {
+            ConvertedValue = c.Convert(Conversion.ConversionMode.Celsius_to_Fahrenheit, InputValue);
+          } else {
+            ConvertedValue = c.Convert(Conversion.ConversionMode.Celsius_to_Kelvin, InputValue);
+          }
+        } else if (ConvertFrom.Equals("Fahrenheit")) {
+          if (ConvertTo.Equals("Celcius")) {
+            ConvertedValue = c.Convert(Conversion.ConversionMode.Fahrenheit_to_Celsius, InputValue);
+          } else {
+            ConvertedValue = c.Convert(Conversion.ConversionMode.Fahrenheit_to_Kelvin, InputValue);
+          }
+        } else if (ConvertFrom.Equals("Kelvin")) {
+          if (ConvertTo.Equals("Celcius")) {
+            ConvertedValue = c.Convert(Conversion.ConversionMode.Kelvin_to_Celsius, InputValue);
+          } else {
+            ConvertedValue = c.Convert(Conversion.ConversionMode.Kelvin_to_Fahrenheit, InputValue);
+          }
+        }
+      }
+    }
+}
